0e7a7c2a14b8f88980f31c99797de20e61874a86
Matt Skelley <mattskelley.au@gmail.com>
2022-01-30 19:51:36 +0800

    components and props

--- src/index.js
+++ src/index.js
@@ -3,13 +3,28 @@ import ReactDOM from "react-dom";
 // import App from "./App.js";
 // ReactDOM.render(<App />, document.getElementById("root"));
 
-function tick() {
-  const element = 
-  <div>
-    <h1>Hello, world!</h1>
-    <h2>It is {new Date().toLocaleTimeString()}.</h2>
-  </div>;
-  ReactDOM.render(element, document.getElementById('root'));
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
 }
 
-setInterval(tick, 1000);
+// class Welcome extends React.Component {
+//   render() {
+//     return <h1>Hello, {this.props.name}</h1>
+//   }
+// }
+
+// From Reacts point of view, these two components are the same
+
+// Elements can be represented as user defined components
+// const element = <Welcome name="Sara" />;
+
+function App() {
+  return (
+    <div>
+      <Welcome name='Sara'/>
+      <Welcome name='Matt'/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
