d33d7d031840a17fb2b178ff27f490cab043ac0b
Matt Skelley <mattskelley.au@gmail.com>
2022-01-30 19:22:15 +0800

    Introducing JSX

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App.js";
-ReactDOM.render(<App />, document.getElementById("root"));
+// import App from "./App.js";
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
+
+const element = <h1>Hello, {formatName(user)}</h1>;
+
+ReactDOM.render(element, document.getElementById('root'));
