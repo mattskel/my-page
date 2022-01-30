cac9254ac19cd7bd8056289b42edd6a671b0bb96
Matt Skelley <mattskelley.au@gmail.com>
2022-01-30 21:06:13 +0800

    adding lifecycle methods to a class

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { nextTick } from "process";
 import React from "react";
 import ReactDOM from "react-dom";
 // import App from "./App.js";
@@ -18,13 +19,62 @@ function Welcome(props) {
 // Elements can be represented as user defined components
 // const element = <Welcome name="Sara" />;
 
-function App() {
-  return (
-    <div>
-      <Welcome name='Sara'/>
-      <Welcome name='Matt'/>
-    </div>
-  )
+// function App() {
+//   return (
+//     <div>
+//       <Welcome name='Sara'/>
+//       <Welcome name='Matt'/>
+//     </div>
+//   )
+// }
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// function Clock(props) {
+//   return (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {props.date.toLocaleTimeString()}</h2>
+//     </div>
+//   )
+// }
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerId = setInterval(() => this.tick(), 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    })
+  }
+
+  // render gets run each time an update happens
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+      </div>
+    )
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// function tick() {
+//   ReactDOM.render(<Clock date={new Date()}/>, document.getElementById('root'));
+// }
+
+// setInterval(tick, 1000);
+
+// What we actually want
+ReactDOM.render(<Clock />, document.getElementById('root'));
