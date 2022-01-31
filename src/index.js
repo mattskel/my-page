687bfbf108470bf3f798f8a1038a12be527dfb69
Matt Skelley <mattskelley.au@gmail.com>
2022-01-31 19:28:54 +0800

    Conditional rendering

--- src/index.js
+++ src/index.js
@@ -3,23 +3,66 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import App from "./App.js";
 // ReactDOM.render(<App />, document.getElementById("root"));
-class Toggle extends React.Component {
+
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  )
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  )
+}
+
+function UserGreeting(props) {
+  return <h1>Welcome back!</h1>;
+}
+
+function GuestGreeting(props) {
+  return <h1>Please sign up.</h1>;
+}
+
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn
+  if (isLoggedIn) return <UserGreeting />;
+
+  return <GuestGreeting />;
+}
+class LoginControl extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {isToggleOn: true};
+    this.state = {isLoggedIn: false};
 
-    this.handleClick = this.handleClick.bind(this);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
   }
 
-  handleClick() {
-    this.setState((state) => ({isToggleOn: !state.isToggleOn}))
+  handleLoginClick() {
+    this.setState({isLoggedIn: true});
+  }
+
+  handleLogoutClick() {
+    this.setState({isLoggedIn: false})
   }
 
   render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    const button = (isLoggedIn)
+      ? <LogoutButton onClick={this.handleLogoutClick} />
+      : <LoginButton onClick={this.handleLoginClick} />
     return (
-      <button onClick={this.handleClick}>{this.state.isToggleOn ? 'ON' : 'OFF'}</button>
+      <div>
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
+      </div>
     )
   }
 }
 
-ReactDOM.render(<Toggle />, document.getElementById('root'));
+ReactDOM.render(<LoginControl />, document.getElementById('root'));
